1312e89112608ae2b11afffb0834c8845d871321
davidlam-winc-com-au <71114461+davidlam-winc-com-au@users.noreply.github.com>
2020-09-23 14:06:21 +1000

    Update show-column-names.sql

--- show-column-names.sql
+++ show-column-names.sql
@@ -1,3 +1,10 @@
+// warning: this doesn't show ___T columns which are in the rows with PROPERTY_TYPE = 4
+
 SELECT PROPERTY_NAME, PROPERTY_CAPTION, COLUMN_SQL_TYPE FROM "_SYS_BI"."BIMC_PROPERTIES" 
 WHERE  SCHEMA_NAME = '_SYS_BIC' AND "CUBE_NAME" = 'last part of the view' 
 AND "CATALOG_NAME" = 'second last part of the view' AND PROPERTY_TYPE = 1 ORDER BY PROPERTY_NAME
+
+
+// this shows ___T columns but doesn't have the captions
+SELECT COLUMN_NAME, DATA_TYPE_NAME , "LENGTH" FROM sys.VIEW_COLUMNS 
+WHERE SCHEMA_NAME = '_SYS_BIC' AND VIEW_NAME = 'XXX/YYY'  ORDER BY COLUMN_NAME
