dba6ac2fa0b95c1771ba4a262f00b9921a606329
davidlam-at-home <71114461+davidlam-at-home@users.noreply.github.com>
2020-10-05 22:32:56 +1100

    Update rank.sql

--- rank.sql
+++ rank.sql
@@ -3,3 +3,6 @@ select sku, date, sum(qty), rank() OVER (PARTITION by sku ORDER BY sum(qty) desc
 
 // 1, 2,2,3
 select sku, date, sum(qty), dense_rank() OVER (PARTITION by sku ORDER BY sum(qty) desc) AS rank
+
+// 1, 2,3,4 even with duplicate qty
+select sku, date, sum(qty), row_number() OVER (PARTITION by sku ORDER BY sum(qty) desc) AS rank
